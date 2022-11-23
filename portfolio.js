d217bb0905800e545f4e35bd1c29e09c7f672ef8
Mosharraf hosen <116699220+wafid19@users.noreply.github.com>
2022-11-24 03:06:44 +0600

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+       });
+      
+
+
+      // toggle manu/navbar Script
+      $('.btn').click(function(){
+        $('.navbar .manu').toggleClass("active");
+        $('.btn i').toggleClass("active");
+    
+    });
+    //typing animation script
+      var typed = new Typed(".typing", {
+        strings: ["Developer","Designer","Blogger","Engineer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop: true
+
+      });
+
+      var typed = new Typed(".typing-2", {
+        strings: ["Blogger","Developer","Designer","Engineer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop: true
+
+      });
+
+    //owl carousel script
+
+      $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false,
+            },
+            600:{
+                items: 2,
+                nav: false,
+            },
+            1000:{
+                items: 3,
+                nav: false,
+            }
+        }
+      })
+
+    
+});
+
